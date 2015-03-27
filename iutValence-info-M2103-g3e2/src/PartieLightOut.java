3e64ad9f6aaf940f12d8f7220bafadc447a42c4a
pradello <pradello@PC-B122-04.iut-valence.fr>
2015-03-27 17:02:42 +0100

    création des classes Partie

---
+++ iutValence-info-M2103-g3e2/src/PartieLightOut.java
@@ -0,0 +1,19 @@
+/**
+ * permet de jouer au jeu LightOut 
+ * @author MATHEY-PRADELLE
+ */
+public class PartieLightOut {
+
+
+
+/**
+ * méthode permettant de jouer au jeu
+ * arret lorsque toute les cases du plateau sont éteintes
+ * 
+ */
+    public void jouer()
+    {
+    	
+    }
+}
+
